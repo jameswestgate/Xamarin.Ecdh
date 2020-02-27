b18992a11cca53d2e44cc2dad6c10118867be723
James Westgate <jameswestgate@icloud.com>
2020-02-27 17:51:21 +0000

    Basic in-memory Ecdh implementation using Bouncy Castle Portable.

--- Xamarin.Ecdh/MainPage.xaml.cs
+++ Xamarin.Ecdh/MainPage.xaml.cs
@@ -1,10 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Security;
 using Xamarin.Forms;
 
 namespace Xamarin.Ecdh
@@ -14,38 +14,60 @@ namespace Xamarin.Ecdh
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        const string EcdhAlgorithm = "ECDH"; //What do you think about the other algorithms?
+        const int EcdhKeyBitSize = 256;
+        const int EcdhDefaultPrimeProbability = 30;
+        
         public MainPage()
         {
             InitializeComponent();
         }
 
+        //Based on https://codereview.stackexchange.com/questions/110952/bouncycastle-diffie-hellman
         void Button_Clicked(System.Object sender, System.EventArgs e)
         {
-            Console.WriteLine($"Button {sender} was pressed.");
+            //BEGIN SETUP ALICE
+            IAsymmetricCipherKeyPairGenerator aliceKeyGen = GeneratorUtilities.GetKeyPairGenerator("ECDH");
+            DHParametersGenerator aliceGenerator = new DHParametersGenerator();
+            aliceGenerator.Init(EcdhKeyBitSize, EcdhDefaultPrimeProbability, new SecureRandom());
+            DHParameters aliceParameters = aliceGenerator.GenerateParameters();
 
-            using (ECDiffieHellmanCng dhBob = new ECDiffieHellmanCng())
+            KeyGenerationParameters aliceKGP = new DHKeyGenerationParameters(new SecureRandom(), aliceParameters);
+            aliceKeyGen.Init(aliceKGP);
+
+            AsymmetricCipherKeyPair aliceKeyPair = aliceKeyGen.GenerateKeyPair();
+            IBasicAgreement aliceKeyAgree = AgreementUtilities.GetBasicAgreement(EcdhAlgorithm);
+            aliceKeyAgree.Init(aliceKeyPair.Private);
+
+            //END SETUP ALICE
+
+            /////AT THIS POINT, Alice's Public Key, Alice's Parameter P and Alice's Parameter G are sent unsecure to BOB
+
+            //BEGIN SETUP BOB
+            IAsymmetricCipherKeyPairGenerator bobKeyGen = GeneratorUtilities.GetKeyPairGenerator(EcdhAlgorithm);
+            DHParameters bobParameters = new DHParameters(aliceParameters.P, aliceParameters.G);
+
+            KeyGenerationParameters bobKGP = new DHKeyGenerationParameters(new SecureRandom(), bobParameters);
+            bobKeyGen.Init(bobKGP);
+
+            AsymmetricCipherKeyPair bobKeyPair = bobKeyGen.GenerateKeyPair();
+            IBasicAgreement bobKeyAgree = AgreementUtilities.GetBasicAgreement(EcdhAlgorithm);
+            bobKeyAgree.Init(bobKeyPair.Private);
+            //END SETUP BOB
+
+            BigInteger aliceAgree = aliceKeyAgree.CalculateAgreement(bobKeyPair.Public);
+            BigInteger bobAgree = bobKeyAgree.CalculateAgreement(aliceKeyPair.Public);
+
+            if (!aliceAgree.Equals(bobAgree))
             {
-                dhBob.KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash;
-                dhBob.HashAlgorithm = CngAlgorithm.Sha256;
-                string xmlBob = Crypto.ToXmlString(dhBob.PublicKey);
-
-                Console.WriteLine(xmlBob);
-
-                using (ECDiffieHellmanCng dhAlice = new ECDiffieHellmanCng())
-                {
-                    dhAlice.KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash;
-                    dhAlice.HashAlgorithm = CngAlgorithm.Sha256;
-                    ECDiffieHellmanPublicKey keyBob = Crypto.FromXmlString(xmlBob, dhAlice.KeySize);
-                    byte[] b = dhAlice.DeriveKeyMaterial(keyBob);
-
-
-                    string xmlAlice = Crypto.ToXmlString(dhAlice.PublicKey);
-                    ECDiffieHellmanPublicKey keyAlice = Crypto.FromXmlString(xmlAlice, dhBob.KeySize);
-                    byte[] b2 = dhBob.DeriveKeyMaterial(keyAlice);
-
-                    Console.WriteLine(b.SequenceEqual(b2));
-                }
+                throw new Exception("Keys do not match.");
             }
+
+            KeyParameter aliceSharedKey = new KeyParameter(aliceAgree.ToByteArrayUnsigned());
+            KeyParameter bobSharedKey = new KeyParameter(bobAgree.ToByteArrayUnsigned());
+
+            Console.WriteLine($"ALICE SHARED KEY:{Convert.ToBase64String(aliceSharedKey.GetKey())}");
+            Console.WriteLine($"BOB SHARED KEY:{Convert.ToBase64String(bobSharedKey.GetKey())}");
         }
     }
 }
