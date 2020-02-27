759d4f3e4c8180bc51340aaaf6d068c407e65da3
James Westgate <jameswestgate@icloud.com>
2020-02-27 19:22:19 +0000

    Begin work to exchange values and keys out of band.

--- Xamarin.Ecdh/MainPage.xaml.cs
+++ Xamarin.Ecdh/MainPage.xaml.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.ComponentModel;
+using Org.BouncyCastle.Asn1.Pkcs;
+using Org.BouncyCastle.Asn1.X509;
 using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Crypto.Generators;
 using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Pkcs;
 using Org.BouncyCastle.Security;
+using Org.BouncyCastle.X509;
 using Xamarin.Forms;
 
 namespace Xamarin.Ecdh
@@ -17,7 +21,10 @@ namespace Xamarin.Ecdh
         const string EcdhAlgorithm = "ECDH"; //What do you think about the other algorithms?
         const int EcdhKeyBitSize = 256;
         const int EcdhDefaultPrimeProbability = 30;
-        
+
+
+        private IBasicAgreement _aliceKeyAgree;
+
         public MainPage()
         {
             InitializeComponent();
@@ -26,7 +33,56 @@ namespace Xamarin.Ecdh
         //Based on https://codereview.stackexchange.com/questions/110952/bouncycastle-diffie-hellman
         void Button_Clicked(System.Object sender, System.EventArgs e)
         {
-            //BEGIN SETUP ALICE
+            try
+            {
+                // Setup Alice's parameters and return the public information in base64 format
+                var aliceValues = SetupAlice();
+
+                if (!aliceValues.StartsWith("ECDH:")) throw new ApplicationException("Public Keys Values supplied are not in a valid format.");
+                var splits = (aliceValues.Substring(5).Split(","));
+                var aliceP = new BigInteger(Convert.FromBase64String(splits[0]));
+                var aliceG = new BigInteger(Convert.FromBase64String(splits[1]));
+
+                var publicKeyDerRestored = Convert.FromBase64String(splits[2]);
+                var alicePbk = PublicKeyFactory.CreateKey(publicKeyDerRestored);
+
+                /////AT THIS POINT, Alice's Public Key, Alice's Parameter P and Alice's Parameter G are sent unsecure to BOB
+
+                //BEGIN SETUP BOB
+                IAsymmetricCipherKeyPairGenerator bobKeyGen = GeneratorUtilities.GetKeyPairGenerator(EcdhAlgorithm);
+                DHParameters bobParameters = new DHParameters(aliceP, aliceG);
+
+                KeyGenerationParameters bobKGP = new DHKeyGenerationParameters(new SecureRandom(), bobParameters);
+                bobKeyGen.Init(bobKGP);
+
+                AsymmetricCipherKeyPair bobKeyPair = bobKeyGen.GenerateKeyPair();
+                IBasicAgreement bobKeyAgree = AgreementUtilities.GetBasicAgreement(EcdhAlgorithm);
+                bobKeyAgree.Init(bobKeyPair.Private);
+                //END SETUP BOB
+
+                //Alice and Bob can individually calculate a shared key
+                BigInteger aliceAgree = _aliceKeyAgree.CalculateAgreement(bobKeyPair.Public);
+                BigInteger bobAgree = bobKeyAgree.CalculateAgreement(alicePbk);
+
+                if (!aliceAgree.Equals(bobAgree))
+                {
+                    throw new Exception("Keys do not match.");
+                }
+
+                KeyParameter aliceSharedKey = new KeyParameter(aliceAgree.ToByteArrayUnsigned());
+                KeyParameter bobSharedKey = new KeyParameter(bobAgree.ToByteArrayUnsigned());
+
+                Console.WriteLine($"ALICE SHARED KEY:{Convert.ToBase64String(aliceSharedKey.GetKey())}");
+                Console.WriteLine($"BOB SHARED KEY:{Convert.ToBase64String(bobSharedKey.GetKey())}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"EXCEPTION: {ex}");
+            }
+        }
+
+        private string SetupAlice()
+        {
             IAsymmetricCipherKeyPairGenerator aliceKeyGen = GeneratorUtilities.GetKeyPairGenerator("ECDH");
             DHParametersGenerator aliceGenerator = new DHParametersGenerator();
             aliceGenerator.Init(EcdhKeyBitSize, EcdhDefaultPrimeProbability, new SecureRandom());
@@ -35,39 +91,26 @@ namespace Xamarin.Ecdh
             KeyGenerationParameters aliceKGP = new DHKeyGenerationParameters(new SecureRandom(), aliceParameters);
             aliceKeyGen.Init(aliceKGP);
 
+            //Alice keeps the key agreement variable in memory for now
             AsymmetricCipherKeyPair aliceKeyPair = aliceKeyGen.GenerateKeyPair();
-            IBasicAgreement aliceKeyAgree = AgreementUtilities.GetBasicAgreement(EcdhAlgorithm);
-            aliceKeyAgree.Init(aliceKeyPair.Private);
+            _aliceKeyAgree = AgreementUtilities.GetBasicAgreement(EcdhAlgorithm);
+            _aliceKeyAgree.Init(aliceKeyPair.Private);
 
-            //END SETUP ALICE
+            var tuple1 = Convert.ToBase64String(aliceParameters.P.ToByteArray());
+            var tuple2 = Convert.ToBase64String(aliceParameters.G.ToByteArray());
 
-            /////AT THIS POINT, Alice's Public Key, Alice's Parameter P and Alice's Parameter G are sent unsecure to BOB
+            //Restore as Bigintegers like so
+            //var pRestore = new BigInteger(tuple1);
 
-            //BEGIN SETUP BOB
-            IAsymmetricCipherKeyPairGenerator bobKeyGen = GeneratorUtilities.GetKeyPairGenerator(EcdhAlgorithm);
-            DHParameters bobParameters = new DHParameters(aliceParameters.P, aliceParameters.G);
+            byte[] publicKeyDer = SubjectPublicKeyInfoFactory.CreateSubjectPublicKeyInfo(aliceKeyPair.Public).GetDerEncoded();
+            var tuple3 = Convert.ToBase64String(publicKeyDer);
 
-            KeyGenerationParameters bobKGP = new DHKeyGenerationParameters(new SecureRandom(), bobParameters);
-            bobKeyGen.Init(bobKGP);
+            //https://stackoverflow.com/questions/55976529/how-to-convert-rsa-public-key-to-string-using-bouncycastle-c-sharp
+            //Restore the Public Key like this
+            //byte[] publicKeyDerRestored = Convert.FromBase64String(tuple3);
+            //var temp = PublicKeyFactory.CreateKey(publicKeyDerRestored);
 
-            AsymmetricCipherKeyPair bobKeyPair = bobKeyGen.GenerateKeyPair();
-            IBasicAgreement bobKeyAgree = AgreementUtilities.GetBasicAgreement(EcdhAlgorithm);
-            bobKeyAgree.Init(bobKeyPair.Private);
-            //END SETUP BOB
-
-            BigInteger aliceAgree = aliceKeyAgree.CalculateAgreement(bobKeyPair.Public);
-            BigInteger bobAgree = bobKeyAgree.CalculateAgreement(aliceKeyPair.Public);
-
-            if (!aliceAgree.Equals(bobAgree))
-            {
-                throw new Exception("Keys do not match.");
-            }
-
-            KeyParameter aliceSharedKey = new KeyParameter(aliceAgree.ToByteArrayUnsigned());
-            KeyParameter bobSharedKey = new KeyParameter(bobAgree.ToByteArrayUnsigned());
-
-            Console.WriteLine($"ALICE SHARED KEY:{Convert.ToBase64String(aliceSharedKey.GetKey())}");
-            Console.WriteLine($"BOB SHARED KEY:{Convert.ToBase64String(bobSharedKey.GetKey())}");
+            return $"ECDH:{tuple1},{tuple2},{tuple3}";
         }
     }
 }
