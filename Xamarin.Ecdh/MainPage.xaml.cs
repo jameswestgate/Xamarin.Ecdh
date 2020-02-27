7ce4e1b1b4437280e0f1fb86a543dfeebc9157d3
James Westgate <jameswestgate@icloud.com>
2020-02-27 16:53:56 +0000

    Setup basic test.
    Note, this doesnt compile. Adding System.Security.Cryptography.Cng package does not help either as a type can't be loaded.

--- Xamarin.Ecdh/MainPage.xaml.cs
+++ Xamarin.Ecdh/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -21,6 +22,30 @@ namespace Xamarin.Ecdh
         void Button_Clicked(System.Object sender, System.EventArgs e)
         {
             Console.WriteLine($"Button {sender} was pressed.");
+
+            using (ECDiffieHellmanCng dhBob = new ECDiffieHellmanCng())
+            {
+                dhBob.KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash;
+                dhBob.HashAlgorithm = CngAlgorithm.Sha256;
+                string xmlBob = Crypto.ToXmlString(dhBob.PublicKey);
+
+                Console.WriteLine(xmlBob);
+
+                using (ECDiffieHellmanCng dhAlice = new ECDiffieHellmanCng())
+                {
+                    dhAlice.KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash;
+                    dhAlice.HashAlgorithm = CngAlgorithm.Sha256;
+                    ECDiffieHellmanPublicKey keyBob = Crypto.FromXmlString(xmlBob, dhAlice.KeySize);
+                    byte[] b = dhAlice.DeriveKeyMaterial(keyBob);
+
+
+                    string xmlAlice = Crypto.ToXmlString(dhAlice.PublicKey);
+                    ECDiffieHellmanPublicKey keyAlice = Crypto.FromXmlString(xmlAlice, dhBob.KeySize);
+                    byte[] b2 = dhBob.DeriveKeyMaterial(keyAlice);
+
+                    Console.WriteLine(b.SequenceEqual(b2));
+                }
+            }
         }
     }
 }
